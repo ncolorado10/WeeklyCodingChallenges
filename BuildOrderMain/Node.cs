58b4333376623cbc5190bf8c3682400b6ea2851c
ncolorado10 <97968397+ncolorado10@users.noreply.github.com>
2022-04-21 22:41:37 -0400

    Updated weird formatting Part 2

--- BuildOrderMain/Node.cs
+++ BuildOrderMain/Node.cs
@@ -3,15 +3,15 @@ namespace BuildClass
     public class Node
     {
         public List<Node> Adjacent = new List<Node>();
-	    public char Letter;
-	    public visitStatus visitStatus;
+	public char Letter;
+	public visitStatus visitStatus;
     }
 
     public enum visitStatus
     {
         Unvisited,
-	    Visiting,
-	    Visited	
+	Visiting,
+	Visited	
     }
 
 }
